3a7876772cc35f85cdd4cbf2c62c747b698dbec0
Kofi Doku Atuah <kofidoku.atuah@data61.csiro.au>
2018-10-25 12:00:04 +0200

    Add DTB binding example and GPIO IRQ cascade

--- components/bsp/src/main.c
+++ components/bsp/src/main.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
 #include <assert.h>
+#include <camkes.h>
 
+void eth_nic_gpio_irq0_event_ind(void *foo)
+{
+	(void)foo;
+	eth_nic_irq0_emit();
+}
+
+void eth_nic_gpio_irq0__init(void)
+{
+	int error = eth_nic_gpio_irq0_reg_callback(&eth_nic_gpio_irq0_event_ind, 0);
+	assert(error == 0);
+}
 
--- components/gpio/src/main.c
+++ components/gpio/src/main.c
@@ -6,6 +6,7 @@ void pin_0_to_31_handle(void)
 {
 	int error = pin_0_to_31_acknowledge();
 	assert(error == 0);
+	eth_nic_gpio_irq0_emit();
 }
 
 void pin_32_to_63_handle(void)
